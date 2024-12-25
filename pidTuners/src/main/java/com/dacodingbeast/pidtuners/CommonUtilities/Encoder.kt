c3f2ed9e9d9b5d523e9cbe2a9ce50c1e6d61f19d
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-24 19:38:48 -0700

    added encoder

---
+++ pidTuners/src/main/java/com/dacodingbeast/pidtuners/CommonUtilities/Encoder.kt
@@ -0,0 +1,20 @@
+package com.dacodingbeast.pidtuners.CommonUtilities
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+
+class Encoder(name: String, ahwMap: HardwareMap, encoderDirection: DcMotorSimple.Direction?) {
+    var motor: DcMotorEx = ahwMap.get(DcMotorEx::class.java, name)
+
+    init {
+        motor.direction = encoderDirection
+        motor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motor.mode = DcMotor.RunMode.RUN_USING_ENCODER
+    }
+
+    fun getCurrentPosition(): Int {
+        return motor.currentPosition
+    }
+}
