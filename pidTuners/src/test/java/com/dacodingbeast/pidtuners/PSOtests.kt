0f1b2efbc56afd99a7ab155c125407556ce2ac7f
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-29 20:41:58 -0700

    attempted to fix pso tests

--- pidTuners/src/test/java/com/dacodingbeast/pidtuners/PSOtests.kt
+++ pidTuners/src/test/java/com/dacodingbeast/pidtuners/PSOtests.kt
@@ -7,6 +7,7 @@ import com.dacodingbeast.pidtuners.TypeSpecific.Arm.AngleRange
 import com.dacodingbeast.pidtuners.Algorithm.Particle
 import android.util.Log
 import com.dacodingbeast.pidtuners.Algorithm.FitnessFunction
+import com.dacodingbeast.pidtuners.Algorithm.PSO_Optimizer
 import com.dacodingbeast.pidtuners.HardwareSetup.Motor
 import com.dacodingbeast.pidtuners.Algorithm.Ranges
 import com.dacodingbeast.pidtuners.HardwareSetup.MotorSpecs
@@ -14,6 +15,8 @@ import com.dacodingbeast.pidtuners.HardwareSetup.StallTorque
 import com.dacodingbeast.pidtuners.HardwareSetup.TorqueUnit
 import com.dacodingbeast.pidtuners.Simulators.SimulatorType
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import kotlin.math.PI
 import kotlin.random.Random
@@ -83,6 +86,38 @@ class PSOtests {
         val sim = pso4Arms(constant, angleRanges, 1.0, obstacle, 1.7)
         sim.getPIDFConstants()
     }
+
+        @Test
+        fun testUpdate() {
+            val parameterRanges = arrayListOf(Ranges(0.0, 1.0))
+            val simulationType = SimulatorType.ArmSimulator
+            val time = 1.0
+            val targets = AngleRange.fromRadians(0.0, 1.0)
+            val obstacle = AngleRange.fromRadians(0.5, 0.75)
+
+            val optimizer = PSO_Optimizer(parameterRanges, simulationType, time, targets, obstacle)
+            val result = optimizer.update(10)
+
+            assertNotNull(optimizer.getBest())
+            assertTrue(result >= 0.0)
+        }
+
+        @Test
+        fun testGetBest() {
+            val parameterRanges = arrayListOf(Ranges(0.0, 1.0))
+            val simulationType = SimulatorType.ArmSimulator
+            val time = 1.0
+            val targets = AngleRange.fromRadians(0.0, 1.0)
+            val obstacle = AngleRange.fromRadians(0.5, 0.75)
+
+            val optimizer = PSO_Optimizer(parameterRanges, simulationType, time, targets, obstacle)
+            optimizer.update(10)
+            val bestParticle = optimizer.getBest()
+
+            assertNotNull(bestParticle)
+            assertTrue(bestParticle.bestResult >= 0.0)
+        }
+
 
 
 }
