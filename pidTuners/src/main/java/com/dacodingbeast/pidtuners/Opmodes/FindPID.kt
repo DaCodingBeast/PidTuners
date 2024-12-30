de41e237c3d3f7db3a90c524c1ede69f88f1e5b5
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-29 20:26:49 -0700

    added params for pid

--- pidTuners/src/main/java/com/dacodingbeast/pidtuners/Opmodes/FindPID.kt
+++ pidTuners/src/main/java/com/dacodingbeast/pidtuners/Opmodes/FindPID.kt
@@ -1,16 +1,26 @@
 package com.dacodingbeast.pidtuners.Opmodes
 
 import com.dacodingbeast.pidtuners.Algorithm.PSO_Optimizer
+import com.dacodingbeast.pidtuners.Algorithm.Ranges
+import com.dacodingbeast.pidtuners.CommonUtilities.PivotConstants
+import com.dacodingbeast.pidtuners.Simulators.SimulatorType
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 
 @Autonomous
-class FindPID : LinearOpMode() {
+class FindPID(val constants:PivotConstants,val accuracy:Double,val sim:SimulatorType,val time:Double) : LinearOpMode() {
+
 
     override fun runOpMode() {
 
-        val algorithm = PSO_Optimizer()
+        val algorithm = PSO_Optimizer(arrayListOf(
+            Ranges(0.0, accuracy),
+            Ranges(0.0, accuracy/3.5),
+            Ranges(0.0, accuracy),
+            Ranges(0.0, accuracy)
+        ),sim,time,constants.testingAngle,constants.obstacle)
 
+        waitForStart()
 
         algorithm.update(25)
         telemetry.addLine(algorithm.getBest().toString())
