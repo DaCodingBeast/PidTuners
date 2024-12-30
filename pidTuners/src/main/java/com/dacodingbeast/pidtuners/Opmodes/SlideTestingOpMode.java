23ace1366130a214717995a45b95633d2c0bd16f
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-29 18:50:04 -0700

    added slide testing opmode

---
+++ pidTuners/src/main/java/com/dacodingbeast/pidtuners/Opmodes/SlideTestingOpMode.java
@@ -0,0 +1,36 @@
+package com.dacodingbeast.pidtuners.Opmodes;
+
+import com.dacodingbeast.pidtuners.CommonUtilities.SlideConstants;
+import com.dacodingbeast.pidtuners.HardwareSetup.Motor;
+import com.dacodingbeast.pidtuners.TypeSpecific.Slides.SlideRange;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ArmSpecific.pso4Slides;
+
+public class SlideTestingOpMode extends LinearOpMode {
+    SlideConstants constants;
+    pso4Slides pso4Slides;
+    public SlideTestingOpMode(SlideConstants constants) {
+        this.constants = constants;
+        pso4Slides = new pso4Slides(constants.getSlideSystemConstants(),new ArrayList<>(Arrays.asList(constants.getTarget())),30.0,constants.getObstacle(),3.5 );
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Motor motor = constants.getMotor();
+
+        motor.init(hardwareMap, constants.getTarget().getStart());
+
+        if (constants.getObstacle() != null) {
+        // inverse because we are checking if the angle is in the obstacle range
+        if (!SlideRange.Slides.inRange(constants.getObstacle(), constants.getTarget())) {
+            throw new IllegalArgumentException("Angle " + constants.getTarget() + " is in the obstacle range");
+        }
+
+}
+        waitForStart();
+
+    }
+}
